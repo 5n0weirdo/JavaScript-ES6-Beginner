f41c4732c2f0940fac1128177e0b4382f43d14fd
5n0weirdo <snoweirdo@protonmail.com>
2021-09-30 18:43:10 +0200

    Arrays

---
+++ 1-Basics/6-Arrays/01/index.js
@@ -0,0 +1,16 @@
+let selectedColors = ["red", "blue", "black"];
+console.log(selectedColors);
+console.log(typeof(selectedColors)); //object
+// length of array
+console.log(selectedColors.length); //3
+
+//access index:
+console.log(selectedColors[0]);
+
+//add an element:
+selectedColors[3] = 9;
+console.log(selectedColors);
+
+//change an element:
+selectedColors[2] = "yellow";
+console.log(selectedColors);
