367522751d82486e4de01153187e883d05882f96
5n0weirdo <snoweirdo@protonmail.com>
2021-09-30 05:56:10 +0200

    Basics of ES6

---
+++ Basics/1-Variables/01/index.js
@@ -0,0 +1,6 @@
+let name = "Web";
+console.log(name);
+
+let firstName = "Dev";
+let lastName = "Web";
+console.log(firstName + " " + lastName);
---
+++ Basics/2-Constants/01/index.js
@@ -0,0 +1,3 @@
+// when the value stays the same
+const interestRate = 0.3;
+console.log(interestRate);
