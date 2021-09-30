4e0a5d6196baf1ec140575d524296cec9f18e36b
5n0weirdo <snoweirdo@protonmail.com>
2021-09-30 18:41:21 +0200

    Objects

---
+++ 1-Basics/5-Objects/01/index.js
@@ -0,0 +1,13 @@
+let person = {
+  name: "WebDev",
+  age: 34,
+  position: "Senior"
+}
+
+// access properties: Dot Notation
+person.name = "Designer";
+console.log(person.name);
+
+//access properties: Bracket Notation
+person["age"] = 24;
+console.log(person["age"]);
