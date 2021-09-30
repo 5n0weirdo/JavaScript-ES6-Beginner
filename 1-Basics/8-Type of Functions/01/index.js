296ff969ce25b2858c99b2318181b4d832316e3d
5n0weirdo <snoweirdo@protonmail.com>
2021-09-30 19:03:17 +0200

    Type of Functions

---
+++ 1-Basics/8-Type of Functions/01/index.js
@@ -0,0 +1,12 @@
+//Performing a task
+function greet(personName) {
+  console.log("Hello " + personName);
+}
+greet("Developer");
+greet("WebDev");
+
+//Calculating a value
+function square(num) {
+  return num * num;
+}
+console.log(square(2));
