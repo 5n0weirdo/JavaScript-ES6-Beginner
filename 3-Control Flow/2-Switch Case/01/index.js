fca46a45e0a997643b3cfafdd1e9ff111a28c3dc
5n0weirdo <snoweirdo@protonmail.com>
2021-10-01 18:39:41 +0200

    Switch Case

---
+++ 3-Control Flow/2-Switch Case/01/index.js
@@ -0,0 +1,13 @@
+let role = "Moderator";
+switch(role) {
+  case "Guest":
+  console.log("Guest user");
+  break;
+
+  case "Moderator":
+  console.log("Moderator user");
+  break;
+
+  default:
+  console.log("Unknown User");
+}
