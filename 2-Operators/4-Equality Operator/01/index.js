60cb21c35744b8bbd110119362cedae614562379
5n0weirdo <snoweirdo@protonmail.com>
2021-10-01 09:53:10 +0200

    Equality Operator

---
+++ 2-Operators/4-Equality Operator/01/index.js
@@ -0,0 +1,11 @@
+let x = 2;
+
+//strict equality (Same type + value)
+console.log(x === 2);
+console.log(x === "2")
+console.log(x === true);
+
+//lose equality (Same value)
+console.log(x == 2)
+console.log(x == "2");
+console.log(1 == true);
