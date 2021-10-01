a3e808f434f8e0ebae51a2ac18e8faa16cad2a23
5n0weirdo <snoweirdo@protonmail.com>
2021-10-01 09:51:15 +0200

    Comparison Operators

---
+++ 2-Operators/3-Comparison Operators/01/index.js
@@ -0,0 +1,11 @@
+let x = 10
+
+// relational
+console.log(x < 2); //false
+console.log(x >= 6); //true
+console.log(x < 10); //false
+console.log(x <= 10); //true
+
+//equality
+console.log(x === 1);
+console.log(x !== 10);
