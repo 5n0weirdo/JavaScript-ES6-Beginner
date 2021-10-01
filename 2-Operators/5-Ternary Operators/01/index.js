1f5414f7d67414d4c38cdac3ba80ccce0bf5d3d4
5n0weirdo <snoweirdo@protonmail.com>
2021-10-01 09:54:32 +0200

    Ternary Operator

---
+++ 2-Operators/5-Ternary Operators/01/index.js
@@ -0,0 +1,8 @@
+//if customer has more than 100 points,
+//they are a 'gold' customer
+//otherwise, they are a 'silver' customer
+
+let points = 90;
+let type = points > 100 ? "Gold" : "Silver";
+
+console.log(type);
