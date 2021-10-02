3f113042bd1264a08e34a0ae9ed6b76a1ff1b864
5n0weirdo <snoweirdo@protonmail.com>
2021-10-02 06:14:40 +0200

    Infinite Loops

---
+++ 3-Control Flow/6-Infinite Loops/01/index.js
@@ -0,0 +1,18 @@
+//while loop
+let i = 0;
+ while (i <= 5) {
+   console.log(i);
+   //i++;
+ }
+
+while (true) {
+
+}
+
+//do while loop
+do {
+
+} while(true)
+
+//for loop
+for (let i = 0; i <= 10; )
