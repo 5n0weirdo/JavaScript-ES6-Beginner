e58c19a4ccdfb2402db09b870ba3fcd9cc8c72e0
5n0weirdo <snoweirdo@protonmail.com>
2021-10-02 06:21:16 +0200

    Exercise-Landscape or Portrait

---
+++ 3-Control Flow/11-Exercise-Landscape or Portrait/01/index.js
@@ -0,0 +1,16 @@
+// let widthHeight = isLandscape(300, 200);
+// console.log(widthHeight);
+
+//Or
+
+console.log(isLandscape(100, 200));
+
+function isLandscape(width, height) {
+  // if (width > height) {
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
+  return (width > height);
+}
