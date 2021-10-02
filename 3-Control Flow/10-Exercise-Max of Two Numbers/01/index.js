1e9b3331419b0810157b20b676fe295bf39a72a3
5n0weirdo <snoweirdo@protonmail.com>
2021-10-02 06:19:09 +0200

    Exercise-Max of Two Numbers

---
+++ 3-Control Flow/10-Exercise-Max of Two Numbers/01/index.js
@@ -0,0 +1,12 @@
+let number = maxNumbers(6, 5);
+console.log(number);
+
+function maxNumbers(numOne, numTwo) {
+  // if (numOne > numTwo) {
+  //   return numOne;
+  // }
+  // else {
+  //   return numTwo;
+  // }
+  return (numOne > numTwo) ? numOne: numTwo;
+}
