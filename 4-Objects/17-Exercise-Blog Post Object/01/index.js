171a72f161127c79d802225b06d7f2d23f12c16b
5n0weirdo <snoweirdo@protonmail.com>
2021-10-05 06:17:54 +0200

    Exercise-Blog Post Object

---
+++ 4-Objects/17-Exercise-Blog Post Object/01/index.js
@@ -0,0 +1,12 @@
+const blog = {
+  title: "Best recipes",
+  body: "Paragraphs about specific topic.",
+  author: "Snow",
+  views: 899,
+  comments: [
+    {author: "Tey",body: "Info"},
+    {author: "Key",body: "Instructions"}
+  ],
+  isLive: true
+};
+console.log(blog);
