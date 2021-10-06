4263767890103d9bcb58cbaa5fb6c35575c314d7
5n0weirdo <snoweirdo@protonmail.com>
2021-10-06 18:39:45 +0200

    Removing Elements

---
+++ 5-Arrays/5-Removing Elements/01/index.js
@@ -0,0 +1,13 @@
+const numbers = [2, 4, 6, 8];
+
+//remove from end
+numbers.pop();
+console.log(numbers);
+
+//remove from middle:
+numbers.splice(1, 1);
+console.log(numbers);
+
+//remove from beginning
+numbers.shift();
+console.log(numbers);
