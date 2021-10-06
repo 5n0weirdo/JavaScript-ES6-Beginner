ee4e2739eecfe302989c2bf32a694db1d7777014
5n0weirdo <snoweirdo@protonmail.com>
2021-10-06 09:35:35 +0200

    Finding Elements (Primitive Types)

---
+++ 5-Arrays/2-Finding Elements (Primitive Types)/01/index.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4, 5, 4];
+
+console.log(numbers.indexOf("a"));
+console.log(numbers.indexOf(4));
+
+console.log(numbers.lastIndexOf(4));
+
+//see if element exists in array
+console.log(numbers.includes(1));
