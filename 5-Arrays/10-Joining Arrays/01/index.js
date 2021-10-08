52ae0a898912e9564f4fee385ec6a9675e88f66c
5n0weirdo <snoweirdo@protonmail.com>
2021-10-08 09:32:44 +0200

    Joining Arrays

---
+++ 5-Arrays/10-Joining Arrays/01/index.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3];
+const joined = numbers.join(",");
+
+console.log(joined);
+
+const message = "First message";
+const parts = message.split(" ");
+
+console.log(parts);
+
+const combined = parts.join("-");
+
+console.log(combined);
