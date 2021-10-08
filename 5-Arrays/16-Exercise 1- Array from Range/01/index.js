7c0607cca692b48157c2c93dd9d3250f557632ef
5n0weirdo <snoweirdo@protonmail.com>
2021-10-08 12:10:37 +0200

    Exercise 1- Array from Range

---
+++ 5-Arrays/16-Exercise 1- Array from Range/01/index.js
@@ -0,0 +1,11 @@
+
+const arrayFromRange = (min, max) => {
+    const output = [];
+    for (let i = min; i <= max; i++)
+        output.push(i);
+    return output;
+}
+
+const numbers = arrayFromRange(-1, 5);
+
+console.log(numbers);
