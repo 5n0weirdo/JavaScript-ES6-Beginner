11f4d102ad390421753f5ad0f52cf7462f5e9316
5n0weirdo <snoweirdo@protonmail.com>
2021-10-10 06:41:34 +0200

    Let vs Var

---
+++ 6-Functions/9-Let vs Var/01/index.js
@@ -0,0 +1,17 @@
+var color = "red";
+let age = 400;
+
+
+// function start() {
+//     for (var i = 0; i < 5; i++) {
+//         if (true) {
+//             var color = "red";
+//         }
+//     }
+//     console.log(color);
+// }
+
+// //var : function-scoped
+// //ES6 (ES2015): let, const : block-scoped
+
+// start();
